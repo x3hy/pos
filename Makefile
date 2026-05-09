64b5383280531e7b57400c90836804b48ad83854
x3hy <tkf.x1os@gmail.com>
2026-05-09 20:48:11 +1200

    autocommit

--- Makefile
+++ Makefile
@@ -2,9 +2,19 @@ ASM=nasm
 CC=cc
 BUILD_DIR=build
 
+
+all: $(BUILD_DIR)/pos.img commit run
+
 $(BUILD_DIR)/pos.img: $(BUILD_DIR)/main.bin
 	cp $(BUILD_DIR)/main.bin $(BUILD_DIR)/pos.img
 	truncate -s 1440k $(BUILD_DIR)/pos.img
 
 $(BUILD_DIR)/main.bin: main.asm
 	$(ASM) main.asm -f bin -o $(BUILD_DIR)/main.bin
+
+run:
+	qemu-system-i386 -fda $(BUILD_DIR)/pos.img -display curses
+
+commit:
+	git add .
+	git commit -m "autocommit"
--- main.asm
+++ main.asm
@@ -1,11 +1,54 @@
-org 0x7C80
+; pOS bootloader
+org 0x7C00
 bits 16
 
+%define ENDL 0x0D, 0x0A
+
+; prints a string to the screen
+; takes ds:si pointers to a string
+puts:
+	push si
+	push ax
+
+; loop through each char in al
+.loop:
+	lodsb ; loads next char in al
+	or al, al ; check if char is null
+
+	; exit loop
+	jz .done
+
+	; print char to screen and continue loop
+	mov ah, 0x0e
+	int 0x10
+	jmp .loop
+
+.done:
+	pop ax
+	pop si
+	ret
+
 main:
+	; setup data segments
+	mov ax, 0
+	mov ds, ax
+	mov es, ax
+
+	; setup stack
+	mov ss, ax
+	mov sp, 0x7C00
+
+	; print startup message
+	mov si, msg_test
+	call puts
+
+	; exit
 	hlt
 
 .halt:
 	jmp .halt
 
+msg_test: db 'Waddup faggots!', ENDL, 0
+
 times 510-($-$$) db 0
-dw 0AA55h
+dw 0xAA55
